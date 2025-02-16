5593a6c9a5698d0b92aa9d47383fe460aa007176
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-02-15 22:08:29 -0300

    Criado API para a ordem de serviço

---
+++ backend/cb-ordem-servico/src/main/java/br/com/central_bike/cb_ordem_servico/CbOrdemServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.central_bike.cb_ordem_servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbOrdemServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbOrdemServicoApplication.class, args);
+	}
+
+}
---
+++ backend/cb-ordem-servico/src/test/java/br/com/central_bike/cb_ordem_servico/CbOrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.central_bike.cb_ordem_servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbOrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
